6a4adec5ba68088f472e5344a0ddc6b08090dd98
Yura <dedn61@gmail.com>
2018-09-06 11:32:00 +0300

    home work6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {TodoEditComponent} from './components/todo-edit/todo-edit.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import {RoutingModule} from "./routing.module";
+import {NewTodoComponent} from './components/new-todo/new-todo.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+import {NgxSpinnerModule} from "ngx-spinner";
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    TodoEditComponent,
+    NewTodoComponent,
+    LoginComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    RouterModule,
+    RoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {TodoEditComponent} from "./components/todo-edit/todo-edit.component";
+import {NewTodoComponent} from "./components/new-todo/new-todo.component";
+import {LoginComponent} from "./components/login/login.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'new-todo', component: NewTodoComponent},
+  {path: 'todo/:id', component: TodoEditComponent},
+  {path: '**', component: NotFoundComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+
+  ],
+  exports: [RouterModule]
+})
+export class RoutingModule {
+}
